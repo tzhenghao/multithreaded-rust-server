0e2ed2a8512aa021b7e5b5297170c9c6bb714481
Zheng Hao Tan <hi@zhenghaotan.com>
2023-08-10 17:40:44 -0700

    add new new Worker impl

--- src/lib.rs
+++ src/lib.rs
@@ -49,8 +49,10 @@ struct Worker {
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(|| {
-            receiver;
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+            println!("Worker {id} got a job, exiting;");
+            job();
         });
         Worker { id, thread }
     }
